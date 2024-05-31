2e5c3ddfbd13d9d1eb26a9597343c0d768e329d2
Prasanna Babu <prasannababuvijjana@gmail.com>
2024-05-31 22:23:34 +0530

    added new files

---
+++ variables.tf
@@ -0,0 +1,8 @@
+variable "frontend-repo-name"{
+    description = "code commit repository name"
+    type = string
+}
+variable "frontend-bucket-name" {
+  description = "frontend bucket name"
+  type = string
+}
